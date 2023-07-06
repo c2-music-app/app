c0d7b1522aa504bb73d162b81f925a4907d27b83
fares mohalhel <fares.mohalhel@gmail.com>
2023-07-06 15:25:26 +0300

    added user schema

---
+++ server/models/userModel.js
@@ -0,0 +1,65 @@
+// const crypto = require("crypto");
+// const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Please provide a username"],
+    },
+    photo: {
+      type: String,
+      default: "user.png",
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide a email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      minlength: [6, "Please provide a password with min length : 6 "],
+      required: [true, "Please provide a password"],
+      select: false,
+    },
+    role: {
+      type: String,
+      default: "user",
+      enum: ["user", "admin"],
+    },
+    readList: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Story",
+      },
+    ],
+    readListLength: {
+      type: Number,
+      default: 0,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
